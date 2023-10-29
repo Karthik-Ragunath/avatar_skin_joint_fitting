28df922c40c5e85afcebcc0a16f1b45458f61806
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-10-29 07:21:22 -0500

    dev

--- fit_mesh.py
+++ fit_mesh.py
@@ -7,6 +7,10 @@ from types import SimpleNamespace
 from torch.utils.data import SequentialSampler
 from torch.utils.data import BatchSampler
 from typing import List
+from models import MultiFrameAudioAE
+import torch.optim as optim
+import copy
+import os
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -17,6 +21,7 @@ def parse_arguments():
     parser.add_argument("--num_frames", "-n_frames", help="provide number of frames", type=int, required=False, default=60)
     parser.add_argument("--load_saved_model", "-load_model", help="if saved model must be loaded", action='store_true')
     parser.add_argument("--num_condition_frames", "-n_cond", help="provide number of condition frames", type=int, required=False, default=1)
+    parser.add_argument("--epoch_save_interval", "-save_interval", help="provide the eppch interval to save the model", type=int, required=True, default=10)
     args = parser.parse_args()
     return args
 
@@ -25,39 +30,8 @@ def load_mesh(file_path):
     mesh = load_objs_as_meshes([file_path], device=torch.device("cpu"))
     return mesh
 
-def fit_mesh(mesh_src, mesh_tgt, args):
-    start_iter = 0
-    start_time = time.time()
-
-    deform_vertices_src = torch.zeros(mesh_src.verts_packed().shape, requires_grad=True, device='cuda')
-    optimizer = torch.optim.Adam([deform_vertices_src], lr = args.lr)
-    print("Starting training !")
-    for step in range(start_iter, args.max_iter):
-        iter_start_time = time.time()
-
-        new_mesh_src = mesh_src.offset_verts(deform_vertices_src)
-
-        sample_trg = sample_points_from_meshes(mesh_tgt, args.n_points)
-        sample_src = sample_points_from_meshes(new_mesh_src, args.n_points)
-
-        loss_reg = losses.chamfer_loss(sample_src, sample_trg)
-        loss_smooth = losses.smoothness_loss(new_mesh_src)
-
-        loss = args.w_chamfer * loss_reg + args.w_smooth * loss_smooth
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()        
-
-        total_time = time.time() - start_time
-        iter_time = time.time() - iter_start_time
-
-        loss_vis = loss.cpu().item()
-
-        print("[%4d/%4d]; ttime: %.0f (%.2f); loss: %.3f" % (step, args.max_iter, total_time,  iter_time, loss_vis))        
-    
-    mesh_src.offset_verts_(deform_vertices_src)
-    print('Done!')
+def feed_auto_encoder():
+    pass
 
 def load_mesh(file_path: str):
     mesh = load_mesh(file_path)
@@ -68,7 +42,10 @@ def load_mesh(file_path: str):
 def main(args: SimpleNamespace, source_mesh_file_paths: List, target_mesh_file_paths: List):
     frame_size = args.frame_size
     num_epochs = args.num_epochs
+    latent_size = args.latent_size
     num_condition_frames = args.num_condition_frames
+    num_experts = args.num_experts
+    epoch_save_interval = args.epoch_save_interval
 
     future_weights = (
         torch.ones(args.num_future_predictions)
@@ -76,12 +53,23 @@ def main(args: SimpleNamespace, source_mesh_file_paths: List, target_mesh_file_p
         .div_(args.num_future_predictions)
     )
 
+    pose_auto_encoder = MultiFrameAudioAE(
+        frame_size,
+        latent_size,
+        num_condition_frames,
+        num_experts,
+    ).to(args.device)
+
+    vae_optimizer = optim.Adam(pose_auto_encoder.parameters(), lr=args.initial_lr)
 
     # buffer for later
     shape = (args.mini_batch_size, args.num_condition_frames, frame_size)
-    history = torch.empty(shape).to(args.device)
+    condition = torch.empty(shape).to(args.device)
+    ground_truth = torch.empty(shape).to(args.device)
     for epoch in range(1, num_epochs + 1):
-        for source_mesh_file_path, target_mesh_file_path in zip(source_mesh_file_paths, target_mesh_file_paths):
+        ep_recon_loss = 0
+        mesh_index = 1 # for maintaining coding practice, since mesh_index will be used down the line for division
+        for mesh_index, (source_mesh_file_path, target_mesh_file_path) in enumerate(zip(source_mesh_file_paths, target_mesh_file_paths)):
             source_mesh, source_vertices, source_faces = load_mesh(source_mesh_file_path)
             target_mesh, target_vertices, target_faces = load_mesh(target_mesh_file_path)
             selectable_indices = list(range(args.num_condition_frames, source_mesh.shape[0], 1))
@@ -90,9 +78,39 @@ def main(args: SimpleNamespace, source_mesh_file_paths: List, target_mesh_file_p
                 args.mini_batch_size,
                 drop_last=True,
             )
+            mini_batch_index = 1 # for maintaining coding practice, since mini_batch_index will be used down the line for division
+            for mini_batch_index, indices in enumerate(sampler):
+                t_indices = torch.LongTensor(indices)
+                condition_range = (
+                    t_indices.repeat((args.num_condition_frames, 1)).t()
+                    - torch.arange(args.num_condition_frames - 1, -1, -1).long()
+                )
+                condition[:, :args.num_condition_frames].copy_(source_vertices[condition_range])
+                ground_truth[:, :args.num_condition_frames].copy_(target_vertices[condition_range])
+                vae_output, recon_loss = feed_auto_encoder(
+                    pose_auto_encoder, ground_truth, condition, future_weights
+                )
+                vae_optimizer.zero_grad()
+                (recon_loss).backward()
+                vae_optimizer.step()
+
+                ep_recon_loss += float(recon_loss)
+        
+        avg_ep_recon_loss = ep_recon_loss / (mini_batch_index * mesh_index)
+        print(f"Average Reconstruction for epoch: {epoch} is {avg_ep_recon_loss}")
+        if epoch % epoch_save_interval == 0:
+            os.makedirs("save_model", exist_ok=True)
+            torch.save(copy.deepcopy(pose_auto_encoder).cpu(), os.path.join("save_model", f"{epoch}.pt"))
+
+
+
 
 if __name__ == "__main__":
     args = parse_arguments()
+    args.num_epochs = 100
+    args.mini_batch_size = 64
+    args.initial_lr = 1e-4
+    args.final_lr = 1e-7
     # setup parameters
     mesh = load_mesh(args.file_path)
     vertices = mesh.verts_list()[0]
